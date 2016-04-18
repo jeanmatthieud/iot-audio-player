f918742a73548c2b08b79ae52e841245e9885242
Jean-Matthieu DECHRISTE <jeanmatthieu@dechriste.fr>
2016-04-18 21:39:26 +0200

    Déplacement de la librairie de son

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "ESP8266WiFi.h"
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
